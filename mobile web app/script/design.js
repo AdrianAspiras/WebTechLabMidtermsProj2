3468d4ac92c267a21925799d98e1fbacb8b64b4d
Mike <2151736@slu.edu.ph>
2017-04-08 18:51:15 +0800

    added some script and css

---
+++ mobile web app/script/design.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/* show and hide div for adding of notes*/
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if (e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
+}
--- mobile web app/script/noteScript.js
+++ mobile web app/script/noteScript.js
@@ -1,17 +1,45 @@
-notes = "{'notes':[{'descTitle':'Missed Topics','description':'- Farleigh is gay','class':'IT322-MWF(10:30-11:30)-9351B'},{'descTitle':'Missed Topics','description':'- Farleigh is still gay', 'class':'IT321-MWF(11:30-12:30)-9351A'}]}";
-	notes = notes.replace(/'/g,"\"");
-	notesJ = JSON.parse(notes);
-	localStorage.setItem("Notes",notes);
+	//notes = "{'notes':[{'descTitle':'Missed Topics','description':'- Farleigh is gay','class':'IT322-MWF(10:30-11:30)-9351B','rem':{'alert':'false','dateT':'2017-04-11T18:45'}},{'descTitle':'Missed Topics','description':'- Farleigh is still gay', 'class':'IT321-MWF(11:30-12:30)-9351A','rem':{'alert':'false','dateT':'2017-04-11T18:45'}}]}";
+	//notes = notes.replace(/'/g,"\"");
+	//notesJ = JSON.parse(notes);
+	//localStorage.setItem("Notes",notes);
+	var modal = document.getElementById('popUp');
+	var span = document.getElementById('close');
+	var locl = "{'classNames':[{'className':'IT321-9351A-11:00'},{'className':'IT321-9351A-11:00'},{'className':'IT322-9351B-10:30'}]}"
+		locl = locl.replace(/'/g,"\"");
+	localStorage.setItem("classNames",locl);
+
+	function appendList(){
+			//var classes = "{'classNames':[{'cName':'geishit'},{'cName':'izreligei'}]}";
+			    //classes = classes.replace(/'/g,"\""); 
+			//var classNem = JSON.parse(classes);
+			var classNem = JSON.parse(localStorage.getItem("classNames"));
+			console.log(classNem.classNames.length)
+			for(var count = 0; count < classNem.classNames.length; count++){
+				var opt = document.createElement("option");
+				document.getElementById("classSelect").append(opt);
+				document.getElementById("classSelect").lastChild.innerHTML += classNem.classNames[count].className;			
+			}
+	}
+
+	appendList();
 
 	function dispNotes(){
-	notesJ = JSON.parse(localStorage.getItem("Notes"));
-	console.log(notesJ.notes[0].descTitle);
+		if (localStorage.getItem("Notes") === null)  {
+ 			document.getElementById("disNotes").innerHTML = "<h2>There are no posted notes</h2>"
+ 		}
+ 		var lclStrg = localStorage.getItem("Notes");
+ 		notesJ = JSON.parse(lclStrg);
+
+ 		if (notesJ.notes.length === 0)  {
+ 			document.getElementById("disNotes").innerHTML = "<h2>There are no posted notes</h2>"
+ 		}
+		notesJ = JSON.parse(lclStrg);
 
 	for (var i = notesJ.notes.length - 1; i >= 0; i--) {
 		let tit = notesJ.notes[i].descTitle;
 		let clss = notesJ.notes[i].class;
 		let desc = notesJ.notes[i].description;
-		hotmail =  "<div class='note'><h3>Title: "+tit+"</h3><h4>Class: "+clss+"</h4><p>Description: "+desc+"</p><div class='submitbut'><input type='button' class='button' name='Delet' value='Delete Note' onclick='deletNote("+i+")'></div></div>";
+		hotmail =  "<div class='note'><h3>"+tit+"</h3><h4>"+clss+"</h4><p>"+desc+"</p><input type='button' name='Delet' value='Delete' onclick='deletNote("+i+")'></div>";
 		hotmail = hotmail.replace(/'/g,"\"");
 		document.getElementById("disNotes").innerHTML += hotmail;
 		
@@ -27,19 +55,7 @@ notes = "{'notes':[{'descTitle':'Missed Topics','description':'- Farleigh is gay
 	}
 	dispNotes();
 
-	
-	function appendList(){
-			var classes = "{'classNames':[{'cName':'geishit'},{'cName':'izreligei'}]}";
-			    classes = classes.replace(/'/g,"\""); 
-			var classNem = JSON.parse(classes);
-			console.log(classNem.classNames.length)
-			for(var count = 0; count < classNem.classNames.length; count++){
-				var opt = document.createElement("option");
-				document.getElementById("classSelect").append(opt);
-				document.getElementById("classSelect").lastChild.innerHTML += classNem.classNames[count].cName;			
-			}
-	}
-	appendList();
+
 	function deletNote(indx){
 		var strg = localStorage.getItem("Notes");
 		strg = JSON.parse(strg);
@@ -60,12 +76,26 @@ notes = "{'notes':[{'descTitle':'Missed Topics','description':'- Farleigh is gay
  			var nNote = "{'notes':[]}";
  				nNote = nNote.replace(/'/g,"\"");
  			localStorage.setItem("Notes",nNote);
- 		}
+ 			}
+ 		
 		var title = document.getElementById("noteDet").tit.value;
+			if(document.getElementById("noteDet").tit.value == ""){
+				title = "None";
+			}
 		var watClass = document.getElementById("classSelect").options[document.getElementById("classSelect").selectedIndex].value;
 		var desc = document.getElementById("tArea").value;
-		var newNote = "{'descTitle':'"+title+"','description':'"+desc+"','class':'"+watClass+"'}";
+			desc = desc.replace(/\n/g,' &#13;&#10; ');
+		var remBool;
+			if(document.getElementById("noteDet").yes.checked == true){
+				remBool = true;
+			} else {
+				remBool = false;
+			}
+		var remDate = document.getElementById("noteDet").dateTime.value;
+		console.log(remDate);
+		var newNote = "{'descTitle':'"+title+"','description':'"+desc+"','class':'"+watClass+"','rem':{'alert':'"+remBool+"','dateT':'"+remDate+"'}}";
 			newNote = newNote.replace(/'/g,"\"");
+		console.log(newNote);
 		newNoteJ = JSON.parse(newNote);
 		var savedNote = JSON.parse(localStorage.getItem("Notes"));
 		savedNote.notes.push(newNoteJ);
@@ -75,13 +105,15 @@ notes = "{'notes':[{'descTitle':'Missed Topics','description':'- Farleigh is gay
 			document.getElementById("disNotes").removeChild(document.getElementById("disNotes").firstChild);
 		}
 		dispNotes();
+
+		document.getElementById("noteDet").reset();
 	}
 
-	function hsnote() {
-	    let x = document.getElementById('addNoots');
-	    if (x.style.display === 'none') {
-	        x.style.display = 'block';
-	    } else {
-	        x.style.display = 'none';
-	    }
+	span.onclick = function() {
+	    modal.style.display = "none";
+	    note.notes[indx].rem.alert = false;
+		console.log(note.notes[indx].rem)
+		localStorage.setItem("Notes",note);
 	}
+
+
