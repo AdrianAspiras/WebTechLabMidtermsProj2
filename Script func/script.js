130ffecc97faa8c6029386c94bdab345293b7be8
Isaac Emmanuel Medina <isaacemmanuelmedina@gmail.com>
2017-04-10 18:05:29 +0800

    Added a class detection func.
    Functionality to detect the classes that the use has for the current day

--- Script func/script.js
+++ Script func/script.js
@@ -16,7 +16,7 @@
 		readFromLocalStorage();
 		getVal();
 		selectSortingType();
-
+		setInterval(classesToday,4320000000);
 
 
 
@@ -242,9 +242,9 @@
  			for(var count = 0; count < jsonObj.length; count++){
  				//Do the labels
  				if(count == 0){
- 					document.getElementById("seatPlan").innerHTML = "<input name='student' type='checkbox' id=" + jsonObj[count].ID  + " value=" + jsonObj[count].ID +" />" +  jsonObj[count].Name +" " + "-" + jsonObj[count].ID + "<br />";
+ 					document.getElementById("seatPlan").innerHTML = "<input name='student' type='checkbox' id=" + jsonObj[count].ID  + " value=" + jsonObj[count].ID +" />" + " <label for= " + jsonObj[count].ID +">" +  jsonObj[count].Name +" " + "-" + jsonObj[count].ID + "</label> " + "<br />";
  				}else{
- 					document.getElementById("seatPlan").innerHTML += "<input name='student' type='checkbox'  id=" + jsonObj[count].ID + " value=" + jsonObj[count].ID +" />" +  jsonObj[count].Name +" " + "-" + jsonObj[count].ID + "<br />";
+ 					document.getElementById("seatPlan").innerHTML += "<input name='student' type='checkbox'  id=" + jsonObj[count].ID + " value=" + jsonObj[count].ID +" />" + "<label for= " + jsonObj[count].ID +">" + jsonObj[count].Name +" " + "-" + jsonObj[count].ID + "</label> " + "<br />";
  				}
  			}
  		}
@@ -1125,3 +1125,50 @@
 			displayStudAttendance();
 		}
 	}
+
+	function clearGroups(){
+
+	}
+
+	function classesToday(){
+		var dateToday = new Date();
+		var dayToday = dateToday.getDay();
+		var classes = JSON.parse(localStorage.getItem("Courses"));
+		console.log(classes);
+		var day = "";
+		switch(dayToday){
+			case 0:
+				day = "Sun";
+				break;
+			case 1:
+				day = "Mon";
+				break;
+			case 2:
+				day = "Tue";
+				break;
+			case 3:
+				day = "Wed";
+				break;
+			case 4:
+				day = "Thu";
+				break;
+			case 5:
+				day = "Fri";
+				break;
+			case 6:
+				day = "Sat";
+				break;
+		}
+		console.log(day);
+		hasClass = false;
+		document.getElementById("ClassesToday").innerHTML = "<h3> " + " Your Classes Today "; 
+		for(var count = 0; count < classes.length; count++){
+			var course = JSON.parse(localStorage.getItem(classes[count].CourseKey));
+			console.log(course.Days.indexOf(day));
+			if(course.Days.indexOf(day) != - 1){
+				document.getElementById("ClassesToday").innerHTML += "<h3> " + course.classCode + " " + course.CourseStartT + " at " + course.Room;
+			}		
+		}
+	}
+
+	//Create a semi random grouping
